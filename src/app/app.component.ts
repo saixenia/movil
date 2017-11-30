e82e73333f6d0c3be1ecfee948f85481f6541ef6
unknown <julian-rodriguez92@hotmail.com>
2017-11-29 23:48:25 -0500

    version 1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Photos } from './model/photos';
+import { Albums } from './model/albums';
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  Photos: Array<photos>;
+  Albums: Array<albums>;
+  constructor(private http: Http){
+	this.http.get("https://jsonplaceholder.typicode.com/photos").subscribe(resp=>this.Photos=resp.json());
+	this.http.get("https://jsonplaceholder.typicode.com/albums").subscribe(resp=>this.Photos=resp.json());
+  }
 }
